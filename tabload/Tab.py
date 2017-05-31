38cf971401f83a986cb95b69ec682f9d53ca870e
Adrian <leftshiftlp@gmail.com>
2017-05-31 13:42:20 +0200

    switched to lxml parser, added notes and instrument

--- tabload/Tab.py
+++ tabload/Tab.py
@@ -4,12 +4,13 @@ from bs4 import BeautifulSoup
 
 class Tab():
     """docstring for Tab."""
-    def __init__(self, url, title, artist, rating, type_):
+    def __init__(self, url, title, artist, rating, type_, instrument):
         self.url = url
         self.title = title
         self.artist = artist
         self.rating = rating
         self.type = type_
+        self.instrument = instrument
 
     def load(self):
         self._load_html()
@@ -19,7 +20,7 @@ class Tab():
         self.html = requests.get(self.url).text
 
     def _parse(self):
-        soup = BeautifulSoup(self.html, 'html.parser')
+        soup = BeautifulSoup(self.html, 'lxml')
         self.title = self._parse_title(soup)
         self.artist = self._parse_artist(soup)
         self.album = self._parse_album(soup)
@@ -28,6 +29,7 @@ class Tab():
         self.type = self._parse_type(soup)
         self.rating = self._parse_rating(soup)
         self.text = self._parse_text(soup)
+        self.notes = self._parse_notes(soup)
 
     def _parse_title(self, soup):
         raise NotImplementedError
@@ -47,10 +49,13 @@ class Tab():
     def _parse_type(self, soup):
         raise NotImplementedError
 
+    def _parse_rating(self, soup):
+        raise NotImplementedError
+
     def _parse_text(self, soup):
         raise NotImplementedError
 
-    def _parse_rating(self, soup):
+    def _parse_notes(self, soup):
         raise NotImplementedError
 
     def __repr__(self):
