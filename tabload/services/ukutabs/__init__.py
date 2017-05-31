d934080670b003cd2e3e9b433d1575c11a763f76
Adrian <leftshiftlp@gmail.com>
2017-05-31 13:43:22 +0200

    ukutabs: added Tab page parser

--- tabload/services/ukutabs/__init__.py
+++ tabload/services/ukutabs/__init__.py
@@ -1,3 +1,5 @@
+import re
+
 from tabload.Search import Search as BaseSearch
 from tabload.Tab import Tab as BaseTab
 
@@ -26,9 +28,50 @@ class Search(BaseSearch):
 
 class Tab(BaseTab):
     """docstring for Tab."""
+    r_difficulty = re.compile("Difficulty level ([1-5]/5)")
+
     def __init__(self, url, title, artist, rating, type_):
-        super(Tab, self).__init__(url, title, artist, rating, type_)
+        super(Tab, self).__init__(url, title, artist, rating, type_, "ukulele")
 
     def _parse_title(self, soup):
         td = soup.find('strong', text="Title").parent
         return td.next_sibling.find('span').text
+
+    def _parse_artist(self, soup):
+        td = soup.find('strong', text="Artist").parent
+        return td.next_sibling.text
+
+    def _parse_album(self, soup):
+        td = soup.find('strong', text="Album").parent
+        return td.next_sibling.text
+
+    def _parse_difficulty(self, soup):
+        a = soup.find(class_="kooltip")
+        i = a.find("i")
+        return self.r_difficulty.match(i.string)
+
+    def _parse_capo(self, soup):
+        b = soup.find(class_="boettonon")
+        return b.get("value")
+
+    def _parse_type(self, soup):
+        # Type doesn't appear on actual page
+        # If we got here from search results, just keep that
+        # If we didn't, we simply don't know
+        return self.type
+
+    def _parse_rating(self, soup):
+        # No ratings
+        return None
+
+    def _parse_text(self, soup):
+        import pdb; pdb.set_trace()
+        pre = soup.select_one("div#cont pre")
+        return pre.text
+
+    def _parse_notes(self, soup):
+        pre = soup.find("pre", class_="quotae-code", id="extranote")
+        if pre:
+            return pre.text
+        else:
+            return ""
