51b344ec61bd50531485b3d58c81ca14c0398c48
Adrian Schneider <leftshiftlp@gmail.com>
2017-05-31 21:31:39 +0200

    compleated search

--- tabload/search.py
+++ tabload/search.py
@@ -7,20 +7,20 @@ default_instruments = ['chords']
 
 
 def combined_results(results):
-    for service, search in results.items():
-        if not search:  # All results have been consumed, skip this search
-            continue
+    while True:
+        for service, search in results.items():
+            if not any([s for s in results.values()]):  # All have been died
+                raise StopIteration
 
-        if not any([s for s in results.values()]):  # All have been died
-            raise StopIteration
+            if not search:  # All results have been consumed, skip this search
+                continue
 
-        try:
-            import pdb; pdb.set_trace()
-            yield search.__next__()
-        except StopIteration:
-            # All results for this service have been consumed, we can delete the search
-            results[service] = None
-            continue
+            try:
+                yield search.__next__()
+            except StopIteration:
+                # All results for this service have been consumed, we can delete the search
+                results[service] = None
+                continue
 
 
 def search(query, instruments=default_instruments, services=default_services, items=10):
@@ -37,4 +37,5 @@ def search(query, instruments=default_instruments, services=default_services, it
         page = []
         while len(page) < items:
             page.append(combined.__next__())
+        print(page)
         yield page
