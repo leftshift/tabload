ff3232d99e1a8a3a4500a642d77b3c55c551766f
Adrian <leftshiftlp@gmail.com>
2017-06-14 15:04:19 +0200

    re-added accidentally removed scrolling and pgup/dwn

--- tabload/tab_view.py
+++ tabload/tab_view.py
@@ -8,7 +8,7 @@ class TabView:
         self.window.clear()
         self.window.refresh()
         self.tab = tab
-        if not tab.loaded:
+        if not self.tab.loaded:
             self.tab.load()
 
         clip_box = []
@@ -31,11 +31,31 @@ class TabView:
     def show(self):
         self.pad.refresh(self.posy, self.posx, *self.clip_box)
 
-    def scroll_up(self):
-        self.posy = max(0, self.posy-1)
+    def scroll_up(self, amount=1):
+        self.posy = max(0, self.posy - amount)
         self.show()
 
-    def scroll_down(self):
-        if self.posy + 1 + self.window.getmaxyx()[0] < self.height:
-            self.posy = self.posy+1
-            self.show()
+    def scroll_down(self, amount=1):
+        if self.posy + amount + self.window.getmaxyx()[0] < self.height:
+            self.posy = self.posy + amount
+        else:
+            # Scrolling of amount is not possible, so scroll to a position where everything up to the last line is visible
+            self.posy = self.height - self.window.getmaxyx()[0]
+        self.show()
+
+    def scroll_left(self, amount=1):
+        self.posx = max(0, self.posx - amount)
+        self.show()
+
+    def scroll_right(self, amount=1):
+        if self.posx + amount + self.window.getmaxyx()[1] < self.width:
+            self.posx = self.posx + amount
+        else:
+            self.posx = self.width - self.window.getmaxyx()[1]
+        self.show()
+
+    def page_up(self):
+        self.scroll_up(self.window.getmaxyx()[0])
+
+    def page_down(self):
+        self.scroll_down(self.window.getmaxyx()[0])
