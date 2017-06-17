0ed7eafe0ec4642894f1dcd9801b89c79ffc8f4f
Adrian <leftshiftlp@gmail.com>
2017-06-17 04:29:33 +0200

    added matching groups for whitespace before and after the chord

--- tabload/g.py
+++ tabload/g.py
@@ -2,9 +2,12 @@
 import re
 
 # Matches chords.
-# Group 1: The whole chord (like "Bbm7")
-# Group 2: Just the base note (Like "Bb")
-r_chord = re.compile("\s(([ABCDEFG](?:#{1,2}|b{1,2}))?[1-9]?(?:M|maj|major|m|min|minor|dim|sus|dom|aug|\+|-|add)?[1-9]?)\s")
+# Group 1: Whitespace before the chord
+# Group 2: The whole chord (like "Bbm7")
+# Group 3: Just the base note (Like "Bb")
+# Group 4: The stuff behind the note
+# Group 5: Whitespace after the chord
+r_chord = re.compile("(\s)(([ABCDEFG](?:#{1,2}|b{1,2})?)([1-9]?(?:M|maj|major|m|min|minor|dim|sus|dom|aug|\+|-|add)?[1-9]?))(\s)")
 
 export_format = "text"
 instruments = ['guitar', 'ukulele', 'bass']
